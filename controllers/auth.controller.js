fa1195566d40831028e097250ffef26c016ebcfe
achrams <ramadhanovic@gmail.com>
2026-04-29 17:50:27 +0700

    cors fix preflight

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -75,7 +75,7 @@ exports.login = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      return res.status(403).json({ message: "Invalid credentials" });
     }
 
     if (!user.qr_url) {
